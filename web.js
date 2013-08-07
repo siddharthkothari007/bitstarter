0207ae27343724e6e95524df9ffac60924318dae
siddharthkothari007 <siddharthkothari007@gmail.com>
2013-08-07 15:17:39 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html";
+
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-    
-  response.send(fs.readFileSync('index.html').toString());
+app.get('/', function(request,response){
+        var html = fs.readFileSync(htmlfile).toString();
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+var port = process.env.PORt || 8080;
+app.listen(port,function(){
+    console.log("Listening on " + port);
 });
