be6a88742ed10e5097092f483cbda5003a57cb4d
siddharthkothari007 <siddharthkothari007@gmail.com>
2013-08-11 14:31:38 +0000

    commit web.js

--- web.js
+++ web.js
@@ -10,7 +10,7 @@ app.get('/', function(request, response) {
 //response.send(fs.readFileSync('index.html').toString());
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8099;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
